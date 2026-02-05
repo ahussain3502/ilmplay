ec48cbe334bd745cfbf206b8acb61ba69c1cc51b
ahussain3502 <ahussain3502@gmail.com>
2026-02-05 17:41:52 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("safaplay-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png",
-        "/Untitled design(3).png"
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
