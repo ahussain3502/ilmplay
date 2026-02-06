d2286ceada60e3c1354c166f459be8e90ec431bb
ahussain3502 <ahussain3502@gmail.com>
2026-02-06 14:23:00 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
