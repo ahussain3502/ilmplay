3db849329110a1c399f9b27737a358bf10985f62
ahussain3502 <ahussain3502@gmail.com>
2026-02-16 01:47:04 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,11 @@
-const CACHE_NAME = "ilmplay-v1";
-const CORE = [
-  "/",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE))
-  );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
